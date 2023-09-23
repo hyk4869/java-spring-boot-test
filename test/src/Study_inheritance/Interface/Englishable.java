3d316a03436066196ec35c033fa5f4fd292c5e22
hyk4869 <hyk4869@gmail.com>
2023-09-23 22:58:37 +0900

    インタフェースと例外（途中）

---
+++ test/src/Study_inheritance/Interface/Englishable.java
@@ -0,0 +1,7 @@
+package Study_inheritance.Interface;
+
+public interface Englishable {
+	String LANGUAGE = "英語";
+
+	void displayEng();
+}
---
+++ test/src/Study_inheritance/Interface/StuSample7.java
@@ -0,0 +1,13 @@
+package Study_inheritance.Interface;
+
+public class StuSample7 {
+
+	public static void main(String[] args) {
+		Student7 stu = new Student7("MIKE");
+		stu.display();
+
+		Baseball bb = new Baseball();
+		bb.display();
+	}
+
+}
