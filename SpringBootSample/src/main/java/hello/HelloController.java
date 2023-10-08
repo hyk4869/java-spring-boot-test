56eb2bcdcb9b33ef3ed14a63a886a65011107a5d
hyk4869 <hyk4869@gmail.com>
2023-10-08 15:47:51 +0900

    画面から別画面に値を渡す練習

--- SpringBootSample/src/main/java/hello/HelloController.java
+++ SpringBootSample/src/main/java/hello/HelloController.java
@@ -1,7 +1,10 @@
 package hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,8 +13,28 @@ public class HelloController {
 		return "hello";
 	}
 
+	@PostMapping("/hello")
+	public String postRequest(@RequestParam("text1") String str, Model model) {
+		model.addAttribute("sample", str);
+		return "hello/response";
+	}
 }
 
 // https://rainbow-engine.com/java-spring-boot-404error-fix/
 // Spring Bootで404エラーが出たときの対処
 //　直接原因はコントローラークラス（HewlloController）がスキャンの対象に含まれていなかった。
+
+// @Controller
+// 画面などからHTTPリクエストを受け付けるクラスのことをコントローラー
+
+//@GetMapping
+// GetメソッドのHTTPリクエストを受け付ける
+
+// @PostMapping
+// PostメソッドのHTTPリクエストを受け取るために使う。
+
+// @RequestParam
+// 画面で入力された値をサーバーが受け取る方法
+
+// Model
+// Modelクラスを使うことで、別の画面に値を渡すことができる。
